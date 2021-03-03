3feaf4977272e66fc76e81f2c83ff527593bd2a5
Dmitry Avetisov <dmitry.avetisov@orangesoft.co>
2021-03-03 13:36:55 +0300

    migrate from kotlin synthetic to viewBinding

--- app/src/main/java/co/orangesoft/authmanager/MainActivity.kt
+++ app/src/main/java/co/orangesoft/authmanager/MainActivity.kt
@@ -8,14 +8,13 @@ import androidx.fragment.app.FragmentActivity
 import by.orangesoft.auth.BaseAuthManager
 import by.orangesoft.auth.credentials.AuthCredential
 import by.orangesoft.auth.firebase.credential.Firebase
-import by.orangesoft.auth.firebase.FirebaseUserController
 import by.orangesoft.auth.user.IBaseUserController
 import co.orangesoft.authmanager.auth.SimpleAuthManager
 import co.orangesoft.authmanager.auth.email.EmailAuthCredential
 import co.orangesoft.authmanager.auth.phone.SimplePhoneAuthCredential
+import co.orangesoft.authmanager.databinding.ActivityMainBinding
 import co.orangesoft.authmanager.firebase_auth.AuthManager
 import co.orangesoft.authmanager.firebase_auth.phone_auth.PhoneAuthCredential
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
@@ -26,9 +25,11 @@ class MainActivity : FragmentActivity() {
 
     private val authManager: AuthManager by lazy { AuthManager.getInstance(AuthManager.BASE_URL) }
     private val simpleAuthManager: SimpleAuthManager by lazy { SimpleAuthManager.getInstance(SimpleAuthManager.BASE_URL, applicationContext) }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         initViews()
     }
@@ -47,21 +48,21 @@ class MainActivity : FragmentActivity() {
 
     private fun initViews() {
 
-        googleBtn.setOnClickListener { launchCredential(Firebase.Google(getString(R.string.server_client_id))) }
+        binding.googleBtn.setOnClickListener { launchCredential(Firebase.Google(getString(R.string.server_client_id))) }
 
-        facebookBtn.setOnClickListener { launchCredential(Firebase.Facebook) }
+        binding.facebookBtn.setOnClickListener { launchCredential(Firebase.Facebook) }
 
-        appleBtn.setOnClickListener { launchCredential(Firebase.Apple) }
+        binding.appleBtn.setOnClickListener { launchCredential(Firebase.Apple) }
 
-        phoneBtn.setOnClickListener { launchCredential(PhoneAuthCredential("+375334445566", "1234")) }
+        binding.phoneBtn.setOnClickListener { launchCredential(PhoneAuthCredential("+375334445566", "1234")) }
 
-        simplePhoneBtn.setOnClickListener { launchSimpleCredential(SimplePhoneAuthCredential("+375334445566", "1234")) }
+        binding.simplePhoneBtn.setOnClickListener { launchSimpleCredential(SimplePhoneAuthCredential("+375334445566", "1234")) }
 
-        simpleEmailBtn.setOnClickListener { launchSimpleCredential(EmailAuthCredential("email@gmail.com", "password111")) }
+        binding.simpleEmailBtn.setOnClickListener { launchSimpleCredential(EmailAuthCredential("email@gmail.com", "password111")) }
 
-        simpleRemoveEmailBtn.setOnClickListener { launchSimpleCredential(EmailAuthCredential("email@gmail.com", "password111"), true) }
+        binding.simpleRemoveEmailBtn.setOnClickListener { launchSimpleCredential(EmailAuthCredential("email@gmail.com", "password111"), true) }
 
-        simpleRemovePhoneBtn.setOnClickListener { launchSimpleCredential(SimplePhoneAuthCredential("+375334445566", "1234"), true) }
+        binding.simpleRemovePhoneBtn.setOnClickListener { launchSimpleCredential(SimplePhoneAuthCredential("+375334445566", "1234"), true) }
     }
 
     private fun launchCredential(credential: Firebase) {
@@ -91,7 +92,7 @@ class MainActivity : FragmentActivity() {
         val loginSuccessListener: (IBaseUserController<*>) -> Unit  = {
             val resultCreds = simpleAuthManager.currentUser.value.credentials.value.toString()
             if (resultCreds.isNotEmpty()) {
-                resultCredentials.text = resultCreds
+                binding.resultCredentials.text = resultCreds
             }
         }
 
@@ -99,7 +100,7 @@ class MainActivity : FragmentActivity() {
             Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
             val resultCreds = simpleAuthManager.currentUser.value.credentials.value.toString()
             if (resultCreds.isNotEmpty()) {
-                resultCredentials.text = resultCreds
+                binding.resultCredentials.text = resultCreds
             }
         }
 
