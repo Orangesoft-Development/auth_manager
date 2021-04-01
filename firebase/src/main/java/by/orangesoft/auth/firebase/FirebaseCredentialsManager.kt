60babb61fa2f889f1d05019a84b4bc8722e8f93a
Dmitry Avetisov <dmitry.avetisov@orangesoft.co>
2021-04-01 18:15:45 +0300

    update logic with deprecated dexFile to dexClassLoader

--- firebase/src/main/java/by/orangesoft/auth/firebase/FirebaseCredentialsManager.kt
+++ firebase/src/main/java/by/orangesoft/auth/firebase/FirebaseCredentialsManager.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import by.orangesoft.auth.credentials.*
 import by.orangesoft.auth.firebase.credential.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import dalvik.system.DexFile
+import dalvik.system.DexClassLoader
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.tasks.await
-import java.io.IOException
 import java.util.*
 
 @InternalCoroutinesApi
@@ -28,11 +27,44 @@ open class FirebaseCredentialsManager(appContext: Context, parentJob: Job? = nul
         firebaseInstance
     )
 
+    private fun getCredentialPaths(appContext: Context): ArrayList<String> {
+        val credentials = arrayListOf<String>()
+        val optimizedDirectory = appContext.getDir("outdex", 0).absolutePath
+        val classLoader = DexClassLoader(appContext.packageCodePath, optimizedDirectory, null, DexClassLoader.getSystemClassLoader())
+        if (loadClass(classLoader, APPLE_CLASS_NAME)) { credentials.add(APPLE_CLASS_NAME) }
+        if (loadClass(classLoader, GOOGLE_CLASS_NAME)) { credentials.add(GOOGLE_CLASS_NAME) }
+        if (loadClass(classLoader, FACEBOOK_CLASS_NAME)) { credentials.add(FACEBOOK_CLASS_NAME) }
+
+        return credentials
+    }
+
+    private fun loadClass(classLoader: DexClassLoader, className: String): Boolean {
+        return try {
+            classLoader.loadClass(className)
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    private fun newInstanceOfCredController(classNameWithPath: String?, credential: Firebase.Google? = null): IBaseCredentialController {
+        if (classNameWithPath == null) {
+            throw UnsupportedOperationException("Method is not supported")
+        }
+        val clazz: Class<*> = Class.forName(classNameWithPath)
+        val o = if (classNameWithPath.contains(GOOGLE_CLASS_NAME, true)) {
+            clazz.getConstructor(Firebase.Google::class.java)
+                .newInstance(credential)
+        } else {
+            clazz.newInstance()
+        }
+
+        return o as IBaseCredentialController
+    }
+
     @Throws(Throwable::class)
-    override suspend fun onCredentialAdded(
-        credentialResult: CredentialResult,
-        user: FirebaseUserController
-    ) {
+    override suspend fun onCredentialAdded(credentialResult: CredentialResult, user: FirebaseUserController) {
         user.reloadCredentials()
     }
 
@@ -69,69 +101,17 @@ open class FirebaseCredentialsManager(appContext: Context, parentJob: Job? = nul
         override fun createCredential(): IBaseCredentialController {
 
             return when (credential) {
-                is Firebase.Apple -> newInstanceOfCredController(credPaths.first {
-                    it.contains(
-                        APPLE_CLASS_NAME,
-                        true
-                    )
-                })
-                is Firebase.Google -> newInstanceOfCredController(credPaths.first {
-                    it.contains(
-                        GOOGLE_CLASS_NAME,
-                        true
-                    )
-                }, credential)
-                is Firebase.Facebook -> newInstanceOfCredController(credPaths.first {
-                    it.contains(
-                        FACEBOOK_CLASS_NAME,
-                        true
-                    )
-                })
+                is Firebase.Apple -> newInstanceOfCredController(credPaths.firstOrNull { it.contains(APPLE_CLASS_NAME) })
+                is Firebase.Google -> newInstanceOfCredController(credPaths.firstOrNull { it.contains(GOOGLE_CLASS_NAME) }, credential)
+                is Firebase.Facebook -> newInstanceOfCredController(credPaths.firstOrNull { it.contains(FACEBOOK_CLASS_NAME) })
                 else -> throw UnsupportedOperationException("Method $credential is not supported")
             }
         }
     }
 
     companion object {
-
-        private const val APPLE_CLASS_NAME = "AppleCredentialController"
-        private const val GOOGLE_CLASS_NAME = "GoogleCredentialController"
-        private const val FACEBOOK_CLASS_NAME = "FacebookCredentialController"
-
-        @Suppress("DEPRECATION")
-        fun getCredentialPaths(context: Context): ArrayList<String> {
-            val credentials = arrayListOf<String>()
-
-            try {
-                val df = DexFile(context.packageCodePath)
-                val iter: Enumeration<String> = df.entries()
-                while (iter.hasMoreElements()) {
-                    val type = iter.nextElement()
-
-                    if (type.endsWith(APPLE_CLASS_NAME) ||
-                        type.endsWith(GOOGLE_CLASS_NAME) ||
-                        type.endsWith(FACEBOOK_CLASS_NAME)
-                        ) {
-                            credentials.add(type)
-                        }
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-
-            return credentials
-        }
-
-        fun newInstanceOfCredController(classNameWithPath: String, credential: Firebase.Google? = null): IBaseCredentialController {
-            val clazz: Class<*> = Class.forName(classNameWithPath)
-            val o = if (classNameWithPath.contains(GOOGLE_CLASS_NAME, true)) {
-                clazz.getConstructor(Firebase.Google::class.java)
-                    .newInstance(credential)
-            } else {
-                clazz.newInstance()
-            }
-
-            return o as IBaseCredentialController
-        }
+        private const val APPLE_CLASS_NAME = "co.orangesoft.apple.AppleCredentialController"
+        private const val GOOGLE_CLASS_NAME = "co.orangesoft.google.GoogleCredentialController"
+        private const val FACEBOOK_CLASS_NAME = "co.orangesoft.facebook.FacebookCredentialController"
     }
 }
