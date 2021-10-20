115c854ad0a7d08448abb4bf9c6e1e7544008305
oleg.kotov <ok.incoming@gmail.com>
2021-10-20 16:49:11 +0300

    Update facebook login logic

--- facebook/src/main/java/co/orangesoft/facebook/FacebookCredentialController.kt
+++ facebook/src/main/java/co/orangesoft/facebook/FacebookCredentialController.kt
@@ -1,21 +1,29 @@
 package co.orangesoft.facebook
 
-import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.FragmentActivity
 import by.orangesoft.auth.firebase.credential.FirebaseAuthCredential
 import by.orangesoft.auth.firebase.credential.controllers.BaseFirebaseCredentialController
+import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.LoginStatusCallback
 import com.facebook.internal.CallbackManagerImpl
+import com.facebook.login.LoginBehavior
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.google.firebase.auth.*
 import kotlinx.coroutines.CancellationException
+import java.lang.Exception
 
 class FacebookCredentialController: BaseFirebaseCredentialController(FirebaseAuthCredential.Facebook) {
+
+    private val permissions = listOf("email", "public_profile")
 
     private val callbackFactory: CallbackManager by lazy { CallbackManager.Factory.create() }
 
@@ -30,19 +38,50 @@ class FacebookCredentialController: BaseFirebaseCredentialController(FirebaseAut
 
                 override fun onError(error: FacebookException) { onError("Error add credential ${authCredential.providerId}", error) }
             })
+
         }
     }
 
-    override fun clearCredInfo(context: Context) {
-        super.clearCredInfo(context)
-        loginManager.logOut()
-    }
-
     override fun onProviderCreated(activity: FragmentActivity, activityLauncher: ActivityResultLauncher<Intent>) {
-        loginManager.logIn(activity, listOf("email", "public_profile"))
+        if (isAutoLoginSupport()) {
+            retrieveLoginStatus(activity)
+        } else {
+            logIn(activity)
+        }
     }
 
     override fun onActivityResult(code: Int, data: Intent?) {
         callbackFactory.onActivityResult(CallbackManagerImpl.RequestCodeOffset.Login.toRequestCode(), code, data)
     }
+
+    private fun logIn(activity: FragmentActivity) = loginManager.logIn(activity, permissions)
+
+    private fun retrieveLoginStatus(activity: FragmentActivity) {
+        loginManager.retrieveLoginStatus(activity, object : LoginStatusCallback {
+            override fun onCompleted(accessToken: AccessToken?) {
+                accessToken?.token?.let { onSuccessLogin(it) } ?: run { onError(Exception("accessToken is null")) }
+            }
+
+            override fun onFailure() {
+                Log.i("!!!", "${authCredential.providerId} autoLogin failed, try simple logIn")
+                logIn(activity)
+            }
+
+            override fun onError(exception: Exception?) {
+                exception?.let { onError("Error add credential ${authCredential.providerId}", exception) }
+            }
+        })
+    }
+
+    private fun onSuccessLogin(authToken: String) = emitAuthTask(FacebookAuthProvider.getCredential(authToken))
+
+    private fun isAutoLoginSupport(): Boolean {
+        return try {
+            FacebookSdk.getApplicationContext().packageManager.getApplicationInfo("com.facebook.katana", 0)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }
+
 }
