8926b2c8633ea486c0f89eccbc2bb6ede0156cd2
oleg_ok <ok.incoming@gmail.com>
2022-07-26 12:23:28 +0300

    Fix auth cred providers

--- app/src/main/java/co/orangesoft/authmanager/firebase_auth/phone_auth/PhoneAuthCredential.kt
+++ app/src/main/java/co/orangesoft/authmanager/firebase_auth/phone_auth/PhoneAuthCredential.kt
@@ -2,7 +2,8 @@ package co.orangesoft.authmanager.firebase_auth.phone_auth
 
 import android.annotation.SuppressLint
 import by.orangesoft.auth.firebase.credential.FirebaseAuthCredential
+import by.orangesoft.auth.firebase.credential.FirebaseProviders
 
 @SuppressLint("ParcelCreator")
 data class PhoneAuthCredential(val phone: String, val code: String) :
-    FirebaseAuthCredential("Phone")
+    FirebaseAuthCredential(FirebaseProviders.PHONE)
